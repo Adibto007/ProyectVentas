88fc68dcad63526ab82de9b09225271f17fc81b3
AdibtoSena <jhoanandreshernandesposada@gmail.com>
2024-04-08 17:18:07 -0500

    Formulario

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './AppRouter.jsx'
-import Header from './componentes/header/Header'
-import Carrusel from './componentes/carrusel/carrusel'
-import CardList from './componentes/body/CardList'
-import Footer from './componentes/footer/footer'
-import Registro from './componentes/registro/Registro.jsx'
 import App from './app'
 
 
